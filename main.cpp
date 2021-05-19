9dc57558932401355392133a4e44bd1c29d2ad00
sehe <sgheeren@gmail.com>
2021-05-20 00:33:42 +0200

    Made up some kind of alternative semantics
    Now there's an actual stack.
    
    Sadly, qi::hold cannot do the work for us so we do have to manage stack
    frames painstakingly (also under backtracking).
    
    I may have overcomplicated, because since your explanation of the
    desired behaviour it seems that we don't actually need one. See the
    
    Side notes:
    
     1. I removed the redundancy in mixing in comments with each element of
        the variant. Instead, I just mix in with the variant itself.
    
    Now the program output is (with TRUESTACK undefined):
    
        Input " /*c1*/ obj /*c2*/ type /*c3*/ (
                        /*c4*/ attr /*c5*/: /*c6*/ 10 /*c7*/) "
        Parsed: ObjectDef {obj:"obj" CMTS['/*c1*/'], class_:"type" CMTS['/*c2*/']
            attr name:"attr" CMTS['/*c3*/','/*c4*/']
            attr value:10 CMTS['/*c5*/','/*c6*/','/*c7*/']
        }
        Input " /*c1*/ myobj /*c2*/ sometype /*c3*/ (
                    /*c4*/  attr  /*c5*/:  /*c6*/  10.0    /*c7*/,
                    /*c8*/  attr2 /*c9*/:  /*c10*/ \"LtUaE\" /*c11*/,
                    /*c12*/ attr3 /*c13*/: /*c14*/ ref    (something_else) /*c15*/
                    ) "
        Parsed: ObjectDef {obj:"myobj" CMTS['/*c1*/'], class_:"sometype" CMTS['/*c2*/']
            attr name:"attr" CMTS['/*c3*/','/*c4*/']
            attr value:10 CMTS['/*c5*/','/*c6*/','/*c7*/']
            attr name:"attr2" CMTS['/*c8*/']
            attr value:(tagged:String, LtUaE) CMTS['/*c9*/','/*c10*/','/*c11*/']
            attr name:"attr3" CMTS['/*c12*/']
            attr value:(tagged:Ref, something_else) CMTS['/*c15*/']
        }

--- main.cpp
+++ main.cpp
@@ -2,24 +2,60 @@
 #include <boost/spirit/include/phoenix.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <iomanip>
+#include <deque>
 
 namespace phx = boost::phoenix;
 namespace qi  = boost::spirit::qi;
 
 namespace AST {
-    using Comment      = std::string;
-    using CommentStack = std::vector<Comment>;
+    using Comment  = std::string;
+    using Comments = std::vector<Comment>;
 
     struct CommentMixin {
-        CommentStack comments;
+        virtual ~CommentMixin() = default;
+        Comments comments;
     };
 
-    struct Identifier : public std::string, public CommentMixin {};
-    struct StringVal  : public std::string, public CommentMixin {};
-    struct RefVal     : public std::string, public CommentMixin {};
-    struct DoubleVal  : public CommentMixin { double number; };
+    struct Identifier : std::string, CommentMixin { };
 
-    using AttributeValue = boost::variant<StringVal, DoubleVal, RefVal>;
+    enum class Kind { String, Ref };
+
+    std::ostream& operator<<(std::ostream& os, Kind k) {
+        switch(k) {
+            case Kind::Ref:    return os << "Ref";
+            case Kind::String: return os << "String";
+        }
+        return os << "?";
+    }
+
+    template <Kind kind, typename T>
+    struct Tagged : T {
+        using T::T;
+        using T::operator=;
+
+        friend std::ostream& operator<<(std::ostream& os, Tagged const& v) {
+            return os << "(tagged:" << kind << ", " << static_cast<T const&>(v) << ")";
+        }
+    };
+
+    template <typename T, typename... Mixin>
+    struct Holder : Mixin... {
+        T value;
+
+        template <typename... Init>
+        explicit Holder(Init&&... init) : value(std::forward<Init>(init)...) { }
+
+        friend std::ostream& operator<<(std::ostream& os, Holder const& v) {
+            return ((os << v.value) << ... << static_cast<Mixin const&>(v));
+        }
+    };
+
+    using StringVal  = Holder<Tagged<Kind::String, std::string>>;
+    using RefVal     = Holder<Tagged<Kind::Ref, std::string>>;
+    using DoubleVal  = Holder<double>;
+
+    using Variant        = boost::variant<StringVal, DoubleVal, RefVal>;
+    using AttributeValue = Holder<Variant, CommentMixin>;
 
     struct AttributeDef {
         Identifier name;
@@ -35,31 +71,29 @@ namespace AST {
 
 } // namespace AST
 
+BOOST_FUSION_ADAPT_STRUCT(AST::AttributeDef, name, value)
+BOOST_FUSION_ADAPT_STRUCT(AST::ObjectDef, obj, class_, attributes)
+BOOST_FUSION_ADAPT_TPL_STRUCT((T), (AST::Holder)(T), value)
+BOOST_FUSION_ADAPT_TPL_STRUCT((T)(Mixin), (AST::Holder)(T)(Mixin), value)
+
 namespace AST { // SEHE: debug output helpers
-    std::ostream& dump_comments(std::ostream& os, CommentStack const& cmts)
+    std::ostream& dump(std::ostream& os, Comments const& cmts)
     {
-        os << " CMTS[";
         bool first = true;
         for (Comment const& c : cmts)
-            os << (std::exchange(first, false)? "":",") << std::quoted(c, '\'');
-        return os << "]";
+            os << (std::exchange(first, false) ? "" : ",")
+               << std::quoted(c, '\'');
+        return os;
     }
 
-    std::ostream& operator<<(std::ostream& os, DoubleVal const& o)
+    static std::ostream& operator<<(std::ostream& os, CommentMixin const& cm)
     {
-        return dump_comments(os << o.number, o.comments);
+        return dump(os << " CMTS[", cm.comments) << "]";
     }
 
-    template <typename T>
-    static inline constexpr bool is_commentful_string =
-            std::is_base_of_v<CommentMixin, std::decay_t<T>>
-        and std::is_base_of_v<std::string, std::decay_t<T>>;
-
-    template <typename T>
-    static inline std::enable_if_t<is_commentful_string<T>, std::ostream&>
-    operator<<(std::ostream& os, T const& o)
-    {
-        return dump_comments(os << std::quoted(o), o.comments);
+    static std::ostream& operator<<(std::ostream& os, Identifier const& v) {
+        return os << std::quoted(static_cast<std::string const&>(v))
+                  << static_cast<CommentMixin const&>(v);
     }
 
     static inline std::ostream& operator<<(std::ostream& os, ObjectDef const& o) {
@@ -84,13 +118,63 @@ namespace Parser {
             objectdef =
                 (identifier >> identifier >> '(' >> attributedeflist >> ')');
             attributedeflist = -(attributedef % ',');
-            attributedef     = (identifier >> ':' >> attributeval);
+            attributedef     = identifier >> ':' >> attributeval;
 
-            identifier    = qi::lexeme[identimpl];
-            identimpl     = ((qi::alpha | qi::char_('_')) >>
-                            *(qi::alnum | qi::char_("_.")));
+            auto& s_ = skipper.m_stack;
 
-            attributeval  = refval | stringliteral | doubleval;
+#if TRUESTACK
+            auto enter_ = phx::function{[&s_] { s_.emplace_back(); }};
+            auto leave_ = phx::function{[&s_] { s_.pop_back(); }};
+
+            auto collect_ = phx::function{[&s_](AST::CommentMixin& node) {
+                // think of some useful semantics here?
+                // in particular, when to clear the frame(s)
+                for (auto& frame : s_)
+                    node.comments.insert(node.comments.end(), frame.begin(),
+                                         frame.end());
+            }};
+#else 
+            [[maybe_unused]] auto constexpr specification = R"(
+https://stackoverflow.com/questions/67531913/can-i-collect-attributes-from-my-skipper-parser/67535432?noredirect=1#comment119512956_67535432
+
+Comments should never be duplicated. They should be collected until the
+next AST node is created.Ie.the example:
+
+    /*c1*/ obj /*c2/* type /*c3*/ ( /*c4*/ attr /*c5*/: /*c6*/ 10 /*c7*/)
+
+should result in:
+
+    ObjectDef{
+        Identifier{"myobj", CommentMixin{{"/*c1*/"}}},
+        Identifier{"sometype", CommentMixin{{"/*c2*/"}}},
+        {AttributeDef{
+            {Identifier{"attr",
+                        CommentMixin{{"/*c3*/", "/*c4*"}}}},
+            AttributeValue{DoubleVal{
+                CommentMixin{{"/*c5*/", "/*c6*"}}, 10.0}}}}};
+
+
+Comment /*c7*/ is lost (but would appear on the next Identifier whatever it
+were)
+
+– Jacob Lorensen 14 hours ago
+)";
+            auto enter_   = phx::function{[&s_] { /*s_.emplace_back();*/ }};
+            auto leave_   = phx::function{[&s_] { /*s_.pop_back();*/ }};
+            auto collect_ = phx::function{[&s_](AST::CommentMixin& node) {
+                node.comments.swap(s_.back());
+            }};
+#endif
+
+#define COMMENTFULLY(expr)                                                     \
+    qi::eps[enter_()] >>                                                       \
+        (((expr) >> qi::eps[collect_(_val)]) >> qi::eps[leave_()] |            \
+         qi::eps[leave_()])
+
+            identifier %= COMMENTFULLY((qi::alpha | qi::char_('_')) >>
+                                       *(qi::alnum | qi::char_("_.")));
+
+            attributeval %= COMMENTFULLY(refval | stringliteral | doubleval);
             stringliteral = qi::lexeme['"' >> *~qi::char_('"') >> '"'];
             doubleval     = qi::double_;
             refval        = "ref" >> ('(' >> -identifier >> ')');
@@ -99,25 +183,15 @@ namespace Parser {
                 (objectdef)(attributedeflist)(attributedef)(attributedeflist)(
                     identifier)(attributeval)(stringliteral)(doubleval))
 
-            auto collect = phx::function{[=](AST::CommentMixin& node) {
-                node.comments.clear();
-                node.comments.swap(m_comments);
-            }}(_val);
-
-            on_success(identifier,    collect);
-            on_success(doubleval,     collect);
-            on_success(stringliteral, collect);
-            on_success(refval,        collect);
         }
 
       private:
-        AST::CommentStack m_comments;
-
         struct Skip : qi::grammar<Iterator> {
-            Skip(AST::CommentStack& comments) : Skip::base_type(start), m_comments(comments)
+            Skip() : Skip::base_type(start), m_stack(1)
             {
                 using namespace qi;
-                auto push_comment = phx::push_back(phx::ref(m_comments), _1);
+                auto push_comment =
+                    phx::push_back(phx::back(phx::ref(m_stack)), _1);
 
                 single_line_comment = "//" >> *(char_ - eol) >> (eol | eoi);
                 block_comment       = "/*" >> *(block_comment | char_ - "*/") >> "*/";
@@ -126,12 +200,11 @@ namespace Parser {
                              [push_comment];
             }
 
+            std::deque<AST::Comments> m_stack;
           private:
-            AST::CommentStack& m_comments;
-
             qi::rule<Iterator> start;
             qi::rule<Iterator> block_comment, single_line_comment;
-        } skipper{m_comments};
+        } skipper;
 
         template <typename Attr> using Production = qi::rule<Iterator, Attr(), Skip>;
         template <typename Attr> using Lexeme     = qi::rule<Iterator, Attr()>;
@@ -145,14 +218,10 @@ namespace Parser {
         Production<AST::RefVal>         refval;
 
         // lexemes
-        Lexeme<AST::Identifier> identifier, identimpl;
+        Lexeme<AST::Identifier> identifier;
         Lexeme<AST::StringVal> stringliteral;
     };
 } // namespace Parser
-
-BOOST_FUSION_ADAPT_STRUCT(AST::AttributeDef, name, value)
-BOOST_FUSION_ADAPT_STRUCT(AST::ObjectDef, obj, class_, attributes)
-BOOST_FUSION_ADAPT_STRUCT(AST::DoubleVal, number)
 
 template <typename FwdIt>
 bool parser(FwdIt it, FwdIt end, AST::ObjectDef& objects) {
@@ -161,13 +230,19 @@ bool parser(FwdIt it, FwdIt end, AST::ObjectDef& objects) {
 
 int main()
 {
-    for (std::string const input : {(R"(
-        /*c1*/
-        myobj /*c2*/ sometype /*c3*/ ( /*c4*/ attr /*c5*/: /*c6*/ 10.0 )
-    )")}) {
+    for (std::string const input : {
+             R"~~( /*c1*/ obj /*c2*/ type /*c3*/ (
+                /*c4*/ attr /*c5*/: /*c6*/ 10 /*c7*/) )~~",
+             R"~~( /*c1*/ myobj /*c2*/ sometype /*c3*/ (
+            /*c4*/  attr  /*c5*/:  /*c6*/  10.0    /*c7*/,
+            /*c8*/  attr2 /*c9*/:  /*c10*/ "LtUaE" /*c11*/,
+            /*c12*/ attr3 /*c13*/: /*c14*/ ref    (something_else) /*c15*/
+            ) )~~",
+         }) {
         auto it = input.cbegin(), end = input.end();
         AST::ObjectDef ast;
 
+        std::cout << "Input " << std::quoted(input) << "\n";
         // In ast, the comment /*c6*/ is present 2 times on node DoubleVal(10.0)
         if (parser(it, end, ast)) {
             std::cout << "Parsed: " << ast << "\n";
